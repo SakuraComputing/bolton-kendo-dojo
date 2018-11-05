7cb38b6e45b9c638bed46b374a4283f809f87d39
Ewan Bauld <ewan.bauld@gmail.com>
2018-11-05 15:13:12 +0000

    Add React Router and Menu Component (pass)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import './styles/styles.scss';
+import Menu from './components/Menu';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <LandingPage />
-      </div>
+      <Router>
+        <div className="App">
+          <Menu />
+          <LandingPage />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/Menu.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+
+const Menu = () => {
+  return (
+    <div>
+        <ul className="nav">
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/about">Where and When</Link>
+            </li>
+            <li>
+                <Link to="/history">Club History</Link>
+            </li>
+            <li>
+                <Link to="/whatskendo">What is Kendo</Link>
+            </li>
+            <li>
+                <Link to="/study">Kendo Study</Link>
+            </li>
+            <li>
+                <Link to="/links">Kendo Links</Link>
+            </li>
+            <li>
+                <Link to="/photos">Kendo Photograph's</Link>
+            </li>
+            <li>
+                <Link to="/videos">Kendo Videos</Link>
+            </li>
+        </ul>
+    </div>
+  )
+}
+export default Menu;
