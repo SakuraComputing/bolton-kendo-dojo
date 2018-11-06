98d5f2f42b79c62df4f189cec6602d12df229934
Ewan Bauld <ewan.bauld@gmail.com>
2018-11-06 13:10:00 +0000

    Refactor login test

--- src/tests/components/auth/login.spec.js
+++ src/tests/components/auth/login.spec.js
@@ -2,20 +2,21 @@ import React from 'react';
 import Login from '../../../components/auth/login';
 import { shallow } from 'enzyme';
 
-
 describe('Login Form', () => {
 
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<Login />);
+    })
+
     it('should render correctly', () => {
-        const wrapper = shallow(<Login />);
         expect(wrapper).toMatchSnapshot();
     });
     it('should set the state on change', () => {
-        const email = 'sidsnot@gmail.com';
-        const wrapper = shallow(<Login />);
         wrapper.find('TextFieldGroup').at(0).simulate('change', {
             target: { value: 'Change Function' }
         });
-        console.log(wrapper.state);
         expect(wrapper).toMatchSnapshot();
     });
 });
