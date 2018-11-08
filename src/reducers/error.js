f4e821c7737095568952550e72ad9c719f3e2d66
Ewan Bauld <ewan.bauld@gmail.com>
2018-11-08 21:30:33 +0000

    Remove console.log

--- src/reducers/error.js
+++ src/reducers/error.js
@@ -5,7 +5,6 @@ const inititalState = {};
 export default (state = inititalState, action) => {
     switch(action.type) {
         case GET_ERRORS:
-            console.log('Is this getting fired', action);
             return action.payload;
         default: 
             return state;
