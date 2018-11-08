0dc071fe20658a47d13c7dd5bfa706aeedd5acdc
Ewan Bauld <ewan.bauld@gmail.com>
2018-11-08 17:45:01 +0000

    Correctly write error reducer test (fail)

--- src/tests/reducers/errorReducer.spec.js
+++ src/tests/reducers/errorReducer.spec.js
@@ -1,10 +1,10 @@
-import errorReducer from '../../reducers/auth';
+import errorReducer from '../../reducers/error';
 import { GET_ERRORS } from '../../actions/types';
 
 describe('Error Reducer', () => {
     it('should set the default state', () => {
         const initialState = {};
-        const state = authReducer(undefined, { type: '@@INIT'});
+        const state = errorReducer(undefined, { type: '@@INIT'});
         expect(state).toEqual(initialState);
     });
 });
