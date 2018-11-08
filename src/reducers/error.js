7f0256bf19213d2d47bf1ef407dacd91d0c67048
Ewan Bauld <ewan.bauld@gmail.com>
2018-11-08 18:12:18 +0000

    Add Error Reducer code (pass)

--- src/reducers/error.js
+++ src/reducers/error.js
@@ -1,8 +1,13 @@
+import { GET_ERRORS } from '../actions/types';
+
 const inititalState = {};
 
 export default (state = inititalState, action) => {
     switch(action.type) {
+        case GET_ERRORS:
+            console.log('Is this getting fired', action);
+            return action.payload;
         default: 
-         return state;
+            return state;
     }
 }
--- src/tests/reducers/errorReducer.spec.js
+++ src/tests/reducers/errorReducer.spec.js
@@ -7,6 +7,15 @@ describe('Error Reducer', () => {
         const state = errorReducer(undefined, { type: '@@INIT'});
         expect(state).toEqual(initialState);
     });
+    it('should return a payload when GET_ERRORS is called', () => {
+        const error = 'error';
+        const action = { 
+            type: 'GET_ERRORS',
+            payload: error
+        };
+        const state = errorReducer({}, action);
+        expect(state).toBe(error);
+    });
 });
 
 
