bfd2a5dadc34c81507255d3f1b6c959f56b3565d
Ewan Bauld <ewan.bauld@gmail.com>
2018-11-16 19:20:15 +0000

    Tidy up config

--- src/config/keys_dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb://admin:admin1@ds249372.mlab.com:49372/bolton-kendo",
-    secretOrKey: 'secret',
-    googleAPIKEY: 'AIzaSyADAT64SMWOP7ee4cEOgW8tMYNL673MeDQ',
-    apiMapKey: 'pk.eyJ1IjoiZXdhbmJkIiwiYSI6ImNqb2ozcnEycDAxMjkza3BhbjVpMWd0azUifQ.gRjILY0KoVrvFBY_Hvkv2w'
-}
