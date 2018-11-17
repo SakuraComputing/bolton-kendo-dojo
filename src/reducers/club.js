cfc09e0578667a9e384ded59e7e1fbbcea864e3c
Ewan Bauld <ewan.bauld@gmail.com>
2018-11-17 19:57:14 +0000

    Add basic club reducer component (pass)

---
+++ src/reducers/club.js
@@ -0,0 +1,11 @@
+import { } from '../actions/types';
+
+const initialState = [];
+
+export default (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state
+    }
+
+}
