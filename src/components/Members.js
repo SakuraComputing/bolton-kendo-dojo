9846b2bbf392341b82a5e35aa03590cc77d39739
Ewan Bauld <ewan.bauld@gmail.com>
2018-11-19 23:02:58 +0000

    Apply styling to the Members component.

--- src/components/Members.js
+++ src/components/Members.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const  Members = () => {
   return (
-    <div>
-      <h1>Welcome to the Members Area</h1>
+    <div className="members">
+      <h1 className="members-header">Welcome to the Members Area</h1>
+      <div className="members-area-container">
+        <div className="members-area-link">
+          <Link to="/memberphoto">Photographs</Link>
+          <p>Select the photgraphs to go in to the photgraph section</p>
+        </div>
+        <div className="members-area-link">
+          <Link to="/memberphoto">Kendo Mind Map</Link>
+          <p>Enter the Kendo Mind Map section for all deeper kendo study</p>
+        </div>
+        <div className="members-area-link">
+          <Link to="/memberphoto">Videos</Link>
+          <p>Go into the video section and view private members videos</p>
+        </div>
+        <div className="members-area-link">
+          <Link to="/members">Bulletin Board</Link>
+          <p>Add a post to the bulletin board. Responds to other posts</p>
+        </div>
+      </div>
     </div>
   )
 }
