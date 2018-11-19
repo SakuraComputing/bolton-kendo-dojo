dc867c9c2e28f81d5202f004e79f4ff3f20dcda6
Ewan Bauld <ewan.bauld@gmail.com>
2018-11-19 20:42:52 +0000

    Add spinner failing test to ClubHistory

--- src/tests/components/ClubHistory.spec.js
+++ src/tests/components/ClubHistory.spec.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import ClubHistory from '../../components/ClubHistory';
+import club from '../fixtures/club'; 
 
 describe('Club History Component', () => {
+
+    let clubhistory;
+
+    beforeEach(() => {
+        clubhistory = shallow(<ClubHistory club={club} />)
+    });
+
     it('should render correctly', () => {
-        const clubhistory = shallow(<ClubHistory />)
         expect(clubhistory).toMatchSnapshot();
     });
+
+    it('should not display a spinner when state loading is false', () => {
+        const props = {
+            club: {
+                loading: false,
+                club
+            },
+        }
+        ClubHistory = shallow(<ClubHistory {...props} />)
+        expect(ClubHistory.find('Spinner').length).toEqual(0);
+    });
+
+    it('should display a spinner when state loading is true', () => {
+        const props = {
+            club: {
+                loading: true,
+                club: {}
+            },
+        }
+        clubhistory = shallow(<ClubHistory {...props} />)
+        expect(clubhistory.find('Spinner').length).toEqual(1);
+    });
+
 });
