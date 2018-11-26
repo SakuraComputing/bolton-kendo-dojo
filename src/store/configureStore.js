87d3456807e9ef517deb46866011902af82ba5a2
Ewan Bauld <ewan.bauld@gmail.com>
2018-11-26 19:39:30 +0000

    Add upload reducer to store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,6 +3,7 @@ import thunk from 'redux-thunk';
 import authReducer from '../reducers/auth';
 import errorReducer from '../reducers/error';
 import clubReducer from '../reducers/club';
+import uploadReducer from '../reducers/upload';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -11,7 +12,8 @@ export default () => {
         combineReducers({
             auth: authReducer,
             errors: errorReducer,
-            club: clubReducer
+            club: clubReducer,
+            uploads: uploadReducer
         }),
         composeEnhancers(applyMiddleware(thunk))
     );
