276f294d864e52b62eabc58e0801784b312081da
Ewan Bauld <ewan.bauld@gmail.com>
2018-11-26 16:38:50 +0000

    Write Upload Reducer returning default state (pass)

---
+++ src/reducers/upload.js
@@ -0,0 +1,12 @@
+const initialState = {
+    uploads: {},
+    loading: false
+}
+
+export default (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+
+}
