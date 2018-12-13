832749132f189c911340d9cd4c3b3c3c730f6057
Ewan Bauld <ewan.bauld@gmail.com>
2018-12-13 14:08:51 +0000

    Add photo filter action tests (fail)

---
+++ src/tests/actions/filterActions.spec.js
@@ -0,0 +1,18 @@
+import { setPhotoTextFilter, setPhotoDateSortOrder } from '../../actions/filterActions';
+import { SET_PHOTO_TEXT_FILTER, SORT_PHOTO_BY_DATE } from '../../actions/types';
+
+describe('Filter Actions Object', () => {
+    it('should set the photo text filter object', () => {
+        const action = setPhotoTextFilter('test');
+        expect(action).toEqual({
+            type: SET_PHOTO_TEXT_FILTER,
+            text: 'test'
+        })
+    });
+    it('should set the date sort order object', () => {
+        const action = setPhotoDateSortOrder();
+        expect(action).toEqual({
+            type: SORT_PHOTO_BY_DATE,
+        })
+    });
+});
