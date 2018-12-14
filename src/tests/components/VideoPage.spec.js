2397d140d1f90dc48f9c3919d73ba72c47bab66e
Ewan Bauld <ewan.bauld@gmail.com>
2018-12-14 14:06:32 +0000

    Add videopage tests to render using fixtures, add test for spinner.

--- src/tests/components/VideoPage.spec.js
+++ src/tests/components/VideoPage.spec.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { VideoPage } from '../../components/VideoPage';
+import club from '../fixtures/club'; 
 
 describe('Video Page Component', () => {
 
     let videoPage;
 
     beforeEach(() => {
-        videoPage = shallow(<VideoPage />)
+        videoPage = shallow(<VideoPage club={{club}} />)
     });
     it('should render correctly', () => {
         expect(videoPage).toMatchSnapshot();
     });
+
+    it('should not display a spinner when state loading is false', () => {
+        const props = {
+            club: {
+                loading: false,
+                club
+            },
+        }
+        videoPage = shallow(<videoPage {...props} />)
+        expect(videoPage.find('Spinner').length).toEqual(0);
+    });
+
 });
