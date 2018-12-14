2003b884b2c1dce63c7cf55bfdb7cb970e6d2dac
Ewan Bauld <ewan.bauld@gmail.com>
2018-12-14 02:52:08 +0000

    Add mindmap and styling.

--- src/components/members/KendoMindMap.js
+++ src/components/members/KendoMindMap.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class KendoMindMap extends Component {
+
   render() {
     return (
-      <div className="content-container">
-          <h1>Kendo Mind Map</h1>
+      <div className="content-container mindmap-layout">
+        <img src="/img/background/KendoMindMap.png" alt=""/>
       </div>
     )
   }
