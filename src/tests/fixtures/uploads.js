d303aeec4f569ef0b4ebf62d3d17c49838863cfa
Ewan Bauld <ewan.bauld@gmail.com>
2018-12-19 15:35:51 +0000

    Add title filter selector test (fail)

--- src/tests/fixtures/uploads.js
+++ src/tests/fixtures/uploads.js
@@ -3,18 +3,21 @@ export default [
         "_id": "5bfed7528032901512c0f933",
         "date": "2018-11-28T17:58:42.652Z",
         "filename": "image-1543168184113.png",
+        "title": "Title1",
         "description": "Kendo Photo Details",
     },
     {
         "_id": "5bfed7528036661512c0f933",
         "date": "2018-11-21T17:58:42.652Z",
         "filename": "image-0000000000.png",
+        "title": "Title2",
         "description": "More Details",
     },
     {
         "_id": "5bfed7528032901512c05rtd",
         "date": "2018-11-11T17:58:42.652Z",
         "filename": "image-3333333333.png",
+        "title": "Title3",
         "description": "Extra Details",
     }
 ]
--- src/tests/selectors/photos.spec.js
+++ src/tests/selectors/photos.spec.js
@@ -13,4 +13,16 @@ describe('photo selector', () => {
             uploads: [uploads[0]]
         });
     });
+    it('should filter photo by title', () => {
+        const filters = {
+            photoText: '',
+            photoTitle: 'Title3',
+            photoSortyBy: 'date'
+        }
+        const result = selectorPhotos(uploads, filters);
+        expect(result).toEqual({
+            loading: false,
+            uploads: [uploads[2]]
+        });        
+    });
 });
