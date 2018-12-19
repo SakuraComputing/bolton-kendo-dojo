4566e6db409cb83cce0f598a8edb472ff210e71d
Ewan Bauld <ewan.bauld@gmail.com>
2018-12-19 15:24:45 +0000

    Add photo filter title action (pass)

--- src/actions/filterActions.js
+++ src/actions/filterActions.js
@@ -1,9 +1,14 @@
-import { SET_PHOTO_FILTER_TEXT, SORT_PHOTO_BY_DATE } from './types';
+import { SET_PHOTO_FILTER_TEXT, SET_PHOTO_FILTER_TITLE, SORT_PHOTO_BY_DATE } from './types';
 
 export const setPhotoTextFilter = (photoText = '') => ({
     type: SET_PHOTO_FILTER_TEXT,
     photoText
 });
+
+export const setPhotoFilterTitle = (photoTitle = '') => ({
+    type: SET_PHOTO_FILTER_TITLE,
+    photoTitle
+})
 
 export const setPhotoDateSortOrder = () => ({
     type: SORT_PHOTO_BY_DATE
