fe73ea11225f08281ba4f2573ea955184e34e820
Ewan Bauld <ewan.bauld@gmail.com>
2018-12-19 15:22:01 +0000

    Add Photo Filter Title Action Test (Fail)

--- src/tests/actions/filterActions.spec.js
+++ src/tests/actions/filterActions.spec.js
@@ -1,5 +1,5 @@
-import { setPhotoTextFilter, setPhotoDateSortOrder } from '../../actions/filterActions';
-import { SET_PHOTO_FILTER_TEXT, SORT_PHOTO_BY_DATE } from '../../actions/types';
+import { setPhotoTextFilter, setPhotoFilterTitle, setPhotoDateSortOrder } from '../../actions/filterActions';
+import { SET_PHOTO_FILTER_TEXT, SET_PHOTO_FILTER_TITLE, SORT_PHOTO_BY_DATE } from '../../actions/types';
 
 describe('Filter Actions Object', () => {
     it('should set the photo text filter object', () => {
@@ -22,4 +22,13 @@ describe('Filter Actions Object', () => {
             type: SORT_PHOTO_BY_DATE,
         })
     });
+    it('should set the photo title filter object', () => {
+        const titleText = 'Title';
+        const action = setPhotoFilterTitle(titleText);
+        expect(action).toEqual({
+            type: SET_PHOTO_FILTER_TITLE,
+            photoTitle: titleText
+        })
+    });
+
 });
