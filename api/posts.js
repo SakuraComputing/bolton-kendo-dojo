11662f824f25f1e2c2d94e36b72d54ea6f691da9
Ewan Bauld <ewan.bauld@gmail.com>
2018-12-21 16:35:26 +0000

    Add route for single post

--- api/posts.js
+++ api/posts.js
@@ -24,12 +24,20 @@ router.get('/', (req, res) => {
     Post.find()
     .sort({ date: -1 })
     .then(posts => res.json(posts))
-    .catch(err => res.status(404));
+    .catch(err => res.status(404).json({ nopostsfound: "No posts found"}));
+})
+
+// @route GET api/posts/:id
+// @desc Get post by id
+// @access Public
+router.get('/:id', (req, res) => {
+    Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err => res.status(404).json({ nopostfound: "No post found with that Id"}));
 })
 
 
-
-//@route POST api/postst
+// @route POST api/posts
 // @desc Create post route
 // @access Private
 router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
