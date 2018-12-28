66e2a972abf1dbe3e2fee63daa85c647f3038e42
Ewan Bauld <ewan.bauld@gmail.com>
2018-12-28 18:24:14 +0000

    Add PostItem test

---
+++ src/tests/components/members/posts/PostItem.spec.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import PostItem from '../../../components/members/posts/PostItem';
+
+describe('Post Item Component', () => {
+
+    let postItem; 
+
+    beforeEach(() => {
+        postItem = shallow(<PostItem />)
+    })
+    it('should render correctly', () => {
+        expect(postItem).toMatchSnapshot();   
+    });
+});
