9826e30bc2c32f66e1fb692905861c7249cb57cb
Ewan Bauld <ewan.bauld@gmail.com>
2018-12-29 17:25:13 +0000

    Add basic component contents and test for postItem.

--- src/components/members/posts/PostItem.js
+++ src/components/members/posts/PostItem.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import { Link } from 'react-router-dom';
+import { className } from '../../../../node_modules/postcss-selector-parser';
 
 export class PostItem extends Component {
     render() {
+
+        const { post, auth } = this.props;
+
         return (
             <div>
-                
+                <div>
+                    <p>{post.name}</p>
+                </div>
+                <div>
+                    <p>{post.text}</p>
+                    <Link to={`/post/${post._id}`} className="item-comments">
+                        Comments
+                    </Link>
+                    <button 
+                        type="button"
+                        className="item-delete"
+                    >
+                    </button>
+                </div>
             </div>
         )
     }
--- src/tests/components/members/posts/PostItem.spec.js
+++ src/tests/components/members/posts/PostItem.spec.js
@@ -7,9 +7,18 @@ describe('Post Item Component', () => {
     let postItem; 
 
     beforeEach(() => {
-        postItem = shallow(<PostItem />)
+        const post = jest.fn();
+        postItem = shallow(<PostItem post={post}/>)
     })
     it('should render correctly', () => {
         expect(postItem).toMatchSnapshot();   
+    });
+
+    it('should display a delete button', () => {
+        expect(postItem.find('.item-delete').exists()).toBe(true);
+    });
+
+    it('should display a comments button', () => {
+        expect(postItem.find('.item-comments').exists()).toBe(true);
     });
 });
