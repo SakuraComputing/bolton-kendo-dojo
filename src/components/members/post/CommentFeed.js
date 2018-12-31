330f750f7bc7177663cd5161b03c0bdeac40fc58
Ewan Bauld <ewan.bauld@gmail.com>
2018-12-31 17:59:39 +0000

    Add Skeleton components for Post, Post Form, PostComment and PostItem. (pass)

---
+++ src/components/members/post/CommentFeed.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class commentFeed extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default commentFeed;
---
+++ src/components/members/post/CommentForm.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class commentForm extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default commentForm;
