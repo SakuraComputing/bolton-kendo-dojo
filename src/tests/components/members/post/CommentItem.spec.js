1f6468a13a6be63b902eb2d5c86a244c65ee613e
Ewan Bauld <ewan.bauld@gmail.com>
2019-01-02 20:43:33 +0000

    Add CommentItem and Test (pass)

--- src/tests/components/members/post/CommentItem.spec.js
+++ src/tests/components/members/post/CommentItem.spec.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import CommentItem from '../../../../components/members/post/CommentItem';
+import { CommentItem } from '../../../../components/members/post/CommentItem';
 
 describe('Comment Item Component', () => {
-  let commentItem;
+  let commentItem, comment, auth;
 
   beforeEach(() => {
-      commentItem = shallow(<CommentItem />)
+      comment = jest.fn();
+      auth = { user: jest.fn() };
+      commentItem = shallow(<CommentItem comment={comment} auth={auth}/>)
   });
   it('should render correctly', () => {
       expect(commentItem).toMatchSnapshot();
