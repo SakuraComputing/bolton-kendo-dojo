366e076079d97f07bc0e14b94f08eb2ff797ac78
Ewan Bauld <ewan.bauld@gmail.com>
2019-02-12 19:56:34 +0000

    Updates

---
+++ precache-manifest.01e017230aeb3113177d87e4700c6f18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc6ba538864e7a8a600b",
+    "url": "/bolton-kendo-dojo/static/js/runtime~main.fc6ba538.js"
+  },
+  {
+    "revision": "c583d858ee698533e651",
+    "url": "/bolton-kendo-dojo/static/js/main.c583d858.chunk.js"
+  },
+  {
+    "revision": "3f191bffd8ef80659312",
+    "url": "/bolton-kendo-dojo/static/js/1.3f191bff.chunk.js"
+  },
+  {
+    "revision": "c583d858ee698533e651",
+    "url": "/bolton-kendo-dojo/static/css/main.66d41e66.chunk.css"
+  },
+  {
+    "revision": "89931596962e3a6b996212b3b6aa7aef",
+    "url": "/bolton-kendo-dojo/index.html"
+  }
+];
