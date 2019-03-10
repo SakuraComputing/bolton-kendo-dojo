f65db5ed713343966ff8f8c99e5c3e6111065d15
Ewan Bauld <ewan.bauld@gmail.com>
2019-03-10 21:58:07 +0000

    Update server.js to point at build directory not dist

--- server.js
+++ server.js
@@ -47,9 +47,9 @@ let port;
 if(process.env.NODE_ENV === 'production') {
     console.log('Directory Name: ', __dirname);
     port = process.env.PORT || 80;
-    app.use(express.static(path.join(__dirname + '/dist')));
+    app.use(express.static(path.join(__dirname + '/build')));
     app.get('/*', (req,res) => {
-        res.sendFile(path.join(__dirname, '/dist/index.html'));
+        res.sendFile(path.join(__dirname, '/build/index.html'));
     });
 } else {
     port = process.env.PORT || 5000;
