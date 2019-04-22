37546f8dcebcd3bb52df7d0916824cf118890c34
Ewan Bauld <ewan.bauld@gmail.com>
2019-04-22 23:16:37 +0100

    Add REACT_APP variable

--- src/config/keys_prod.js
+++ src/config/keys_prod.js
@@ -1,5 +1,5 @@
 module.exports = {
     mongoURI: process.env.MONGO_URI,
     secretOrKey: process.env.SECRET_OR_KEY,
-    apiMapKey: process.env.MAPBOX_API_KEY
+    apiMapKey: process.env.REACT_APP_MAPBOX_API_KEY
 };
