04d1016cf13678e7ec4a022970ff15e705f1362b
Ewan Bauld <ewan.bauld@gmail.com>
2019-08-01 21:08:06 +0100

    Add debug to keys file

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,4 +1,5 @@
 if(process.env.NODE_ENV === 'production') {
+    console.log('What is NODE_ENV', process.env.NODE_ENV);
     module.exports = require('./keys_prod.js');
 } else {
     console.log('What is NODE_ENV', process.env.NODE_ENV);
