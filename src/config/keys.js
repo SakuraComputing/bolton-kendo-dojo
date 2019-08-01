98dc8ab4d4ea836e27a94fcd4918e74fe5abe523
Ewan Bauld <ewan.bauld@gmail.com>
2019-08-01 20:22:13 +0100

    Add debug and upgrade node and yarn

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,6 +1,7 @@
 if(process.env.NODE_ENV === 'production') {
     module.exports = require('./keys_prod.js');
 } else {
+    console.log('What is NODE_ENV', process.env.NODE_ENV);
     module.exports = require('./keys_dev.js');
 }
 
