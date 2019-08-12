46fa608b6be6a683729904fd9e4d83336e30e0d9
Ewan Bauld <ewan.bauld@gmail.com>
2019-08-12 22:37:18 +0100

    Amend server.js resolve path

--- server.js
+++ server.js
@@ -52,12 +52,14 @@ app.use('/api/posts', posts);
 
 let port;
 
+console.log('Directory:........', path.join(__dirname + '/build','/index.html'));
+
 if(process.env.NODE_ENV === 'production') {
     port = process.env.PORT || 80;
-    app.use(express.static(path.join(__dirname + 'build')));
+    app.use(express.static(path.join(__dirname + '/build')));
 
     app.get('*', (req,res) => {
-        res.sendFile(path.join(__dirname, 'build','index.html'));
+        res.sendFile(path.join(__dirname, '/build','/index.html'));
     });
 } else {
     port = process.env.PORT || 5000;
