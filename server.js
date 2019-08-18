35503c312e55c5410257ff05384790f4fbbcdd15
Ewan Bauld <ewan.bauld@gmail.com>
2019-08-18 21:23:41 +0100

    Add postbuild script to package,json

--- server.js
+++ server.js
@@ -61,8 +61,7 @@ if(process.env.NODE_ENV === 'production') {
     app.get('*', (req,res) => {
         res.sendFile(path.join(__dirname, 'build','index.html'));
     });
-} 
-
-port = process.env.PORT || 5001;
-
+} else {
+    port = process.env.PORT || 5000;
+}
 app.listen(port, () => console.log(`Server running on port ${port}`));
