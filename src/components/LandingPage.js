5041d8847968b08c1183b1e02baea436a45019e9
Ewan Bauld <ewan.bauld@gmail.com>
2019-08-24 01:11:02 +0100

    facebook

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,6 +6,7 @@ const LandingPage = () => {
           <div className="box-layout__box">
           <h1 className="box-layout__title">Bolton Kendo Club</h1>
           <p className="box-layout__synopsis">Welcome to KNMS / Doshinkenyukai</p>
+          <a className="facebook-link" href="https://www.facebook.com/groups/896572830375789/"><img className="facebook" src="/img/facebook.png" alt="facebook"/><div className="facebook-text">Find us on Facebook here</div></a>
         </div>
     </div>
   )
