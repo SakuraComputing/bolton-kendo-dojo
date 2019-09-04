d30dc98993bf7e8aea9944fa3d26001e1c4b62b6
Ewan Bauld <ewan.bauld@gmail.com>
2019-09-04 16:52:55 +0900

    Update styling for the bka logo on desktop

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="bka-details">
-        <a className="footer-text" href="http://www.britishkendoassociation.com" target="_Blank">Member of the British Kendo Association<img src="/img/bka_logo3.png" alt="BKA Logo" className="bka-logo"/></a>
+        <a className="bka-link" href="http://www.britishkendoassociation.com" target="_Blank">Member of the British Kendo Association<img src="/img/bka_logo3.png" alt="BKA Logo" className="bka-logo"/></a>
       </div>
       <div>
         Copyright &copy; {new Date().getFullYear()} Bolton Kendo Club
