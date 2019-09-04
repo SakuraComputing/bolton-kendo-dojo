020340ae99769c4bf520c6948cc2223bae6ba4f3
Ewan Bauld <ewan.bauld@gmail.com>
2019-09-04 16:36:58 +0900

    Add Link to the BKA on the Landing Page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,12 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="footer">
-      Copyright &copy; {new Date().getFullYear()} Bolton Kendo Club
+      <div className="bka-details">
+        <a className="footer-text" href="http://www.britishkendoassociation.com" target="_Blank">Member of the British Kendo Association<img src="/img/bka_logo3.png" alt="BKA Logo" className="bka-logo"/></a>
+      </div>
+      <div>
+        Copyright &copy; {new Date().getFullYear()} Bolton Kendo Club
+      </div>
     </div>
   )
 }
