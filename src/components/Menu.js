455ea419b1f6203f454209ef4e9b13bc1ec89e63
Ewan Bauld <ewan.bauld@gmail.com>
2023-02-06 22:35:51 +0000

    This is an extremely quick hack just to get something up quickly. Node / redux / several components are not needed for this all to work and all needs streamlining when we deploy the proper version

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,80 +1,29 @@
-import React from 'react'
-import { connect } from 'react-redux';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { logoutUser } from '../actions/authActions';
 
 export class Menu extends React.Component {
-
-  onLogoutClick = (e) => {
-      e.preventDefault();
-      this.props.logoutUser();
-  }  
-
-  render() { 
-
-        const { isAuthenticated } = this.props.auth;
-
-        const authLinks = (
-            <div>
-                <li>
-                    <Link to="/study">Kendo Study</Link>
-                </li>
-                <li>
-                    <Link
-                        to="/members"
-                    >
-                    Members
-                    </Link>
-                </li>
-                <li>
-                    <Link 
-                        to="/logout"
-                        href=""
-                        onClick={this.onLogoutClick}
-                    >
-                    Logout
-                    </Link>
-                </li>
-            </div>
-        )
-
-        const guestLinks = (
-            <li>
-                <Link to="/login">Login / Registration</Link>
-            </li>
-        )
-
-        return (
-            <div>
-                <ul className="nav">
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">Where and When</Link>
-                    </li>
-                    <li>
-                        <Link to="/calendar">Calendar</Link>
-                    </li>
-                    <li>
-                        <Link to="/history">Club History</Link>
-                    </li>
-                    <li>
-                        <Link to="/theory">What is Kendo</Link>
-                    </li>
-                    <li>
-                        <Link to="/links">Kendo Links</Link>
-                    </li>
-                    <li>
-                        <Link to="/media">Kendo Media</Link>
-                    </li>
-                    {isAuthenticated ? authLinks :  guestLinks}
-                </ul>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <ul className="nav">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">Where and When</Link>
+          </li>
+          <li>
+            <Link to="/calendar">Calendar</Link>
+          </li>
+          <li>
+            <Link to="/theory">What is Kendo</Link>
+          </li>
+          <li>
+            <Link to="/links">Kendo Links</Link>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
-const mapStateToProps = state => ({
-    auth: state.auth
-})
-export default connect(mapStateToProps, { logoutUser})(Menu);
+export default Menu;
--- src/components/static/About.js
+++ src/components/static/About.js
@@ -1,118 +1,35 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import Map, { Marker, NavigationControl } from 'react-map-gl';
-import Pin from '../../common/Pin';
-import Spinner from '../../common/Spinner';
-
-const API_KEY = require('../../config/keys').apiMapKey;
-
-const LATITUDE = 53.562614;
-const LONGITUDE = -2.384833;
 
 export class About extends React.Component {
-
-  constructor(props) {
-    super(props);
-      this.state = {
-        viewport: {
-            latitude: LATITUDE,
-            longitude: LONGITUDE,
-            zoom: 14,
-            bearing: 0,
-            pitch: 0,
-            width: '100%',
-            height: 400,
-        }
-    };
-  }
-
   render() {
+    console.log('Club Details', this.props.club);
 
-    let clubContent, practises, contacts, address;
-
-    const { viewport } = this.state;
-
-    const { club, loading } = this.props.club;
-
-    if(club !== null && !loading) {
-        practises = club[0].practices.map(practise => (
-            <div key={practise.time}>
-                <div>{practise.venue}</div>
-                <div>{practise.time}</div>
+    const clubContent = (
+      <div>
+        <div className="about-map-section">
+          <div className="about-header">
+            <div className="about-section">
+              <h2>Where we practice</h2>
+              <div>Little Lever School</div>
+              <div>Bolton</div>
+              <div>BL3 1BT</div>
             </div>
-        ));
-        contacts = club[0].contacts.map(contact => (
-            <div key={contact.name}>
-                <div>{contact.name}</div>
-                <div>{contact.telephoneNumber}</div>
-                <div>{contact.email}</div>
+            <div className="about-section">
+              <h2>Practice Times</h2>
+              <div>Sunday 10am</div>
             </div>
-        ));
-        address = club[0].addresses.map(address => (
-            <div key={address.address1}>
-                <div>{address.address1}</div>
-                <div>{address.address2}</div>
-                <div>{address.address3}</div>
-                <div>{address.address4}</div>
-                <div>{address.postcode.slice(0,3) + " " + address.postcode.slice(3,6)}</div>
+            <div className="about-section">
+              <h2>Contact</h2>
+              <div>Mark Needham</div>
             </div>
-        ));
-        clubContent = (
-            <div>
-                <div className="about-map-section" >
-                    <div className="about-header">
-                        <div className="about-section">
-                            <h2>Where we practice</h2>
-                            {address}
-                        </div>
-                        <div className="about-section">
-                            <h2>Practice Times</h2>
-                            {practises}
-                        </div>
-                        <div className="about-section" >
-                            <h2>Contact</h2>
-                            {contacts}
-                        </div>
-                    </div>
-                    <div className="about-map">
-                        <Map
-                            {...viewport}
-                            mapStyle="mapbox://styles/mapbox/streets-v11"
-                            mapboxApiAccessToken={API_KEY}
-                            onViewportChange={viewport => this.setState({viewport})}
-                        >
-                            <div style={{position: 'absolute', right: 0}}>
-                              <NavigationControl />
-                            </div>
-                        >
-                            <Marker 
-                                latitude={LATITUDE} 
-                                longitude={LONGITUDE}
-                            >
-                                <Pin />       
-                            </Marker>
-                        </Map>
-                        
-                    </div>
-                </div>
-                <div>
-                </div>
-            </div>
-        )
-    } else {
-        clubContent = <Spinner />
-    }
-    
-    return (
-        <div className="about-container">
-            {clubContent}
+          </div>
         </div>
-    )
+        <div></div>
+      </div>
+    );
+
+    return <div className="about-container">{clubContent}</div>;
   }
 }
 
-const mapStateToProps = (state) => ({
-    club: state.club
-});
-
-export default connect(mapStateToProps)(About);
+export default About;
