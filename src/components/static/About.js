0b551eaab00497cc777e7a999c35063ce69bbaaa
Ewan Bauld <ewan.bauld@gmail.com>
2023-08-26 20:22:29 +0100

    Add about details

--- src/components/static/About.js
+++ src/components/static/About.js
@@ -11,16 +11,26 @@ export class About extends React.Component {
             <div className="about-section">
               <h2>Where we practice</h2>
               <div>Little Lever School</div>
+              <div>Church Road</div>
               <div>Bolton</div>
               <div>BL3 1BT</div>
+              <div>Tel: 01204 334177</div>
             </div>
             <div className="about-section">
               <h2>Practice Times</h2>
-              <div>Sunday 10am</div>
+              <div>Sunday: 10-12am</div>
+              <div>Parking: Outside free better in the overflow car park</div>
             </div>
             <div className="about-section">
               <h2>Contact</h2>
-              <div>Mark Needham</div>
+              <div>Dojo Leader: Mark Needham (5th Dan)</div>
+              <div>Tel: 01204 778587</div>
+              <span>
+                email:
+                <a href="mailto:markneedham1954@btinternet.com">
+                  markneedham1954@btinternet.com
+                </a>
+              </span>
             </div>
           </div>
         </div>
